802c1cbe1a5785037d6ff0cb0539372b912f7dfd
dnalpert <36242436+dnalpert@users.noreply.github.com>
2019-11-05 14:47:08 -0800

    created component, added select element

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Filter Todos:
+    <select>
+      <option value="200">200</option>
+      <option value="100">100</option>
+      <option value="50">50</option>
+      <option value="5">5</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilterTodos"
+}
+</script>
+
+<style>
+select {
+  margin-top: 20px;
+  padding: 6px;
+  border-top: 1px solid #41b883;
+}
+</style>
