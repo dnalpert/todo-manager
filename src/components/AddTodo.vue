23eaec476c7eb18346693ac3ffa9dfca02946cfa
dnalpert <36242436+dnalpert@users.noreply.github.com>
2019-11-05 14:29:51 -0800

    addTodo complete

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -4,23 +4,31 @@
       Add Todo
     </h3>
     <div class="add">
-      <form>
-        <input type="text" 
-          :model="title"
-          placeholder="Add Todo..."
-        >
-        <input type="submit"
-          value="Submit"
-          
-          >
+      <form @submit="onSubmit">
+        <input type="text" v-model="title" placeholder="Add Todo...">
+        <input type="submit" value="Submit">
       </form>
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
-  name: "AddTodo"
+  name: "AddTodo",
+  data () {
+    return {
+      title: ""
+    }
+  },
+  methods: {
+    ...mapActions(['addTodo']),
+    onSubmit(e) {
+      e.preventDefault()
+      this.addTodo(this.title)
+    }
+  }
 }
 </script>
 
